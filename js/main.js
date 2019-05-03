2bfc6e07d7a4e80c9cb2d5c660513613c88a93ae
Marija Bazovic <mbazovic@live.com>
2019-05-03 11:21:37 +0200

    a2hs

--- js/main.js
+++ js/main.js
@@ -247,6 +247,8 @@ d("direction").addEventListener("click", function () {
 /**
  * Add to home screen - android
  */
+
+// installing service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js')
       .then(function(reg){
@@ -255,3 +257,34 @@ if ('serviceWorker' in navigator) {
         console.log("Neuspelo kreiranje prečice:", err)
     });
  }
+
+// prompt a2hs question inspiration https://github.com/mdn/pwa-examples/tree/master/a2hs
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
