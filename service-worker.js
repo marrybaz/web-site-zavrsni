c6d97929bf4fc4da0d322bd473b3bb7dd1426f4a
Marija Bazovic <mbazovic@live.com>
2019-05-03 18:00:59 +0200

    a2hs bug-fix

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var CACHE_NAME = 'njivica-cache-v1';
 var urlsToCache = [
   '/',
-  'images/dzem.jpg',
-  'images/kupine-slatko.jpg',
-  'css/styles.css',
-  'css/styles-contact.css',
-  'css/styles-products.css',
-  'js/main.js'
+  '/images/dzem.jpg',
+  '/images/kupine-slatko.jpg',
+  '/css/styles.css',
+  '/css/styles-contact.css',
+  '/css/styles-products.css',
+  '/js/main.js'
 ];
 
 self.addEventListener('install', function(event) {
