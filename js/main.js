99a35721f46ffc32cd6679b9f9680f9d68a2e070
Marija Bazovic <mbazovic@live.com>
2019-05-03 11:24:28 +0200

    linting

--- js/main.js
+++ js/main.js
@@ -260,28 +260,28 @@ if ('serviceWorker' in navigator) {
 
 // prompt a2hs question inspiration https://github.com/mdn/pwa-examples/tree/master/a2hs
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+const addBtn = document.querySelector(".add-button");
+addBtn.style.display = "none";
 
-window.addEventListener('beforeinstallprompt', (e) => {
+window.addEventListener("beforeinstallprompt", (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+  addBtn.style.display = "block";
 
-  addBtn.addEventListener('click', (e) => {
+  addBtn.addEventListener("click", (e) => {
     // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
+    addBtn.style.display = "none";
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
+        if (choiceResult.outcome === "accepted") {
+          console.log("Korisnik je prihvatio A2HS");
         } else {
-          console.log('User dismissed the A2HS prompt');
+          console.log("Korisnik NIJE prihvatio A2HS");
         }
         deferredPrompt = null;
       });
