d46c0a77acffe7211c16dd21eafecbc41959da13
Marija Bazovic <mbazovic@live.com>
2019-05-03 17:54:59 +0200

    service worker cache cache

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+var CACHE_NAME = 'njivica-cache-v1';
+var urlsToCache = [
+  '/',
+  'images/dzem.jpg',
+  'images/kupine-slatko.jpg',
+  'css/styles.css',
+  'css/styles-contact.css',
+  'css/styles-products.css',
+  'js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
